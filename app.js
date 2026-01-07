09d630456dd1e05d854ec25effc084a255279098
pradeep <pradeepsable230@gmail.com>
2026-01-07 14:06:38 +0530

    new commenet

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from DevOps Project pradeep sable hi 🚀\n');
+  res.end('Hello from DevOps Project pradeep sable hi all 🚀\n');
 });
 
 server.listen(3000, () => {
